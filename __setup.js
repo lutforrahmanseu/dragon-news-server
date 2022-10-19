8de192fbf7f9b294310f0c417e4f0559c19eb09e
Md.Lutfor Rahman <61869128+lutforrahmanseu@users.noreply.github.com>
2022-10-19 19:47:08 +0600

    categories json api file create

---
+++ __setup.js
@@ -0,0 +1,15 @@
+/*
+Dragon-news-server folder create
+initial knowledge:
+1.Node.js framework express js
+2. express js install
+   1.npm init or npm init -y
+   2.index.js
+   3.npm install express
+   4.nodemon index.js ->server run cmd
+3.data folder create and json api create and the index.js fill apply
+4.
+   
+
+
+*/
